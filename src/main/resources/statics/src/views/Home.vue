e291e200795fa3197877ff8d54ca0f269b3c43de
parkguncherl <luckeey21@gmail.com>
2021-06-12 17:10:53 +0900

    aa

---
+++ src/main/resources/statics/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1> Welcome to Home!</h1>
+  </div>
+</template>
+
+<script>
+//http://localhost/admin/codeList?regId=string&modId=string&regDate=string&modDate=string&codeIdx=0&codeId=string&codeName=string&codeCntn=string
+export default{
+  data(){
+    return {
+      title1:"이천시",
+      title2:"신둔면"
+    };
+  }
+};
+</script>
